21baf0e2bd13164c502dc218bd9b03b08e1254d3
ayssam <ayssam@entratus.ai>
2024-07-25 15:47:17 -0300

    Primeiras funcionalidades

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # MediaCalc
 Aplicação em JavaScript utilizando Node.js que permita a gestão de notas e médias de alunos.
+
+## Objetivo
+Exercício para obtenção de certificado de Introdução a Back-end Development da iTalents.
+
+## Funcionalidades exigidas
+1. Cadastro do Aluno:
+    - [x] Solicitar o nome do aluno.
+2. Cadastro das Matérias:
+    - [x] O aluno pode cadastrar no mínimo 3 matérias.
+    - [x] O cadastro de matérias continua até que o usuário decida parar.
+3. Cadastro de Notas:
+    - [ ] Para cada matéria cadastrada, solicitar 3 notas.
+4. Cálculo de Média:
+    - [ ] Calcular a média individual de cada matéria.
+5. Cadastro e Contabilização de Faltas:
+    - [ ] Solicitar o número de faltas para cada matéria.
+    - [ ] Verificar se o aluno está reprovado por faltas (mais de 5 faltas em qualquer
+matéria).
+6. Resultados:
+    - [ ] Exibir a média de cada matéria.
+    - [ ] Indicar se o aluno está aprovado ou reprovado em cada matéria, considerando
+tanto a média das notas quanto as faltas.
+
---
+++ index.js
@@ -0,0 +1,82 @@
+const user_input = require("prompt-sync")({sigint: true});
+
+function add_subject_validation(answer){
+    if (answer == 's' || answer == 'S'){
+        return 'true';
+    }
+    else if (answer == 'n' || answer == 'N'){
+        return 'false';
+    }
+    else{
+        return 'invalid';
+    }
+}
+
+function get_subjects(){
+    let subject_list = [];
+
+    do{
+        subject = user_input("Adicionar matéria: ");
+        subject_list.push(subject);
+    }while(subject_list.length < 3);
+
+    while(true){
+        answer = user_input("Deseja adicionar mais matérias? (s/n) ");
+        if(add_subject_validation(answer) == 'true'){
+            subject = user_input("Adicionar matéria: ");
+            subject_list.push(subject);
+        }
+        else if(add_subject_validation(answer) == 'false'){
+            break;
+        }
+        else{
+            console.log('Opa. Algo deu errado.');
+            continue;
+        }
+    };
+
+    return subject_list;
+}
+
+function add_grade_validation(grade){
+    if(typeof grade != 'number' || Number.isNaN(grade)){
+        return false
+    }
+
+    return true;
+}
+
+function get_grades(subject){
+    let grades = [];
+    
+    while(grades.length < 3){
+        grade = +user_input(`Digite uma nota de ${subject}: `);
+        if (add_grade_validation(grade)){
+            grades.push(grade);
+            console.log(`Notas de ${subject}: [${grades}]`);
+        }
+        else {
+            console.log('Opa. Algo deu errado.');
+        }
+    }
+
+    return grades;
+}
+
+
+const name = user_input("Digite o nome do aluno: ");
+console.log(`Boletim de ${name}`);
+
+const subject_list = get_subjects();
+console.log(`Matérias = ${subject_list}`);
+
+var subject_info = [];
+
+subject_list.forEach((subject) => {
+    subject_info.push({
+        subject: subject,
+        grades: get_grades(subject)
+    });
+});
+
+console.log(subject_info);
